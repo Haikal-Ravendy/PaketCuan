dd7a107e936a0dd7771dbebb4702726e64e9c73f
Faishal <faishal.rahman9@gmail.com>
2022-10-27 18:54:06 +0700

    register bro

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  username: string;
+  password: string;
+  name: string;
+  avatar: string;
+
+  constructor() {
+    this.username = '';
+    this.name = '';
+    this.password = '';
+    this.avatar = '';
+  }
 
   ngOnInit() {
   }
 
+  submit(){
+    console.log('username: ', this.username);
+    console.log('name: ', this.name);
+    console.log('password: ', this.password);
+    console.log('avatar ', this.avatar);
+    this.username = '';
+    this.name = '';
+    this.password = '';
+    this.avatar = '';
+  }
+
+  async takeImage() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64
+    });
+
+  //   console.log('Result Image', image.base64String);
+  //   this.avatar = `data:image/jpeg;base64, ${image.base64String}`;
+
+  //   // Set avatar value to contact form
+  //   this.contactForm.get('avatar').setValue(this.avatar);
+
+  //   this.cdr.detectChanges();
+  // }
+  }
 }
